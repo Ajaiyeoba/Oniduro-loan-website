eebc5c51232d0dfd82841a73de30866f2dce72e8
Ajaiyeoba <ajaiyeobajibola@gmail.com>
2023-11-11 11:56:16 +0100

    this is the landing page for a loan website named Oniduro.

---
+++ app.js
@@ -0,0 +1,16 @@
+// const menu = document.querySelector("#menu");
+// const navbar = document.querySelector(".navbar");
+
+// menu.addEventListener("click", () => {
+// 	menu.classList.toggle("fa-times");
+// 	navbar.classList.toggle("nav-toggle");
+// });
+
+
+const menu = document.querySelector("#menu");
+const navbar = document.querySelector(".navbar");
+
+menu.addEventListener("click", () => {
+    menu.classList.toggle("img");
+    navbar.classList.toggle("nav-toggle");
+})
